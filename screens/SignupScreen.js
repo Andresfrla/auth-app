7c99d86f44aa13abfab470c4797b4546f05bf046
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2025-05-07 20:33:59 -0600

    Create the conection betwwen firebase and the app

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,28 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUSer } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      await createUSer(email, password);
+    } catch (error) {
+      setIsAuthenticating(false);
+      alert('An error happen creating the user');
+    }
+    setIsAuthenticating(false);
+  }
+  
+
+  if(isAuthenticating){
+    return <LoadingOverlay message="Creating user..."/>
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
--- util/auth.js
+++ util/auth.js
@@ -3,12 +3,18 @@ import axios from "axios";
 const API_KEY = "AIzaSyAl8zNKlA3yZSCNqc_bwW4x385WCPc7Mgc";
 
 export async function createUSer(email, password) {
-  const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
+    try {
+      const response = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      );
+    } catch (error) {
+      console.error('Error creating the user:', error.response?.data || error.message);
+      throw error;
     }
-  );
-}
+  }
+  
